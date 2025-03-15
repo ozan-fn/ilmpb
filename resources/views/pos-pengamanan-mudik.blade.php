6e7a6eefd39b873adecb46b3ae7c162255155475
ozan-fn <23SA11A032@students.amikompurwokerto.ac.id>
2025-03-15 07:37:04 +0700

    modify: structuring

--- resources/views/pos-pengamanan-mudik.blade.php
+++ resources/views/pos-pengamanan-mudik.blade.php
@@ -1,217 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-        <title>Index - Impact Bootstrap Template</title>
-        <meta name="description" content="" />
-        <meta name="keywords" content="" />
+@extends('layouts.app')
 
-        <!-- Favicons -->
-        <link href="assets/img/favicon.png" rel="icon" />
-        <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
+@section('content')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com" rel="preconnect" />
-        <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin />
-        <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-            rel="stylesheet"
-        />
-
-        <!-- Vendor CSS Files -->
-        <link
-            href="assets/vendor/bootstrap/css/bootstrap.min.css"
-            rel="stylesheet"
-        />
-        <link
-            href="assets/vendor/bootstrap-icons/bootstrap-icons.css"
-            rel="stylesheet"
-        />
-        <link href="assets/vendor/aos/aos.css" rel="stylesheet" />
-        <link
-            href="assets/vendor/glightbox/css/glightbox.min.css"
-            rel="stylesheet"
-        />
-        <link
-            href="assets/vendor/swiper/swiper-bundle.min.css"
-            rel="stylesheet"
-        />
-
-        <!-- Main CSS File -->
-        <link href="assets/css/main.css" rel="stylesheet" />
-
-        <!-- =======================================================
-  * Template Name: Impact
-  * Template URL: https://bootstrapmade.com/impact-bootstrap-business-website-template/
-  * Updated: Aug 07 2024 with Bootstrap v5.3.3
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
-    </head>
-
-    <body class="index-page">
-        <header id="header" class="header fixed-top">
-            <!-- <div class="topbar d-flex align-items-center">
-      <div class="container d-flex justify-content-center justify-content-md-between">
-        <div class="contact-info d-flex align-items-center">
-          <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">contact@example.com</a></i>
-          <i class="bi bi-phone d-flex align-items-center ms-4"><span>+1 5589 55488 55</span></i>
+    <div class="hero">
+        <div class="container section-title" data-aos="fade-up">
+            <h2>Pos Pengamanan Mudik<br /></h2>
         </div>
-        <div class="social-links d-none d-md-flex align-items-center">
-          <a href="#" class="twitter"><i class="bi bi-twitter-x"></i></a>
-          <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-          <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-          <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-        </div>
-      </div>
-    </div>End Top Bar -->
 
-            <div class="branding d-flex align-items-cente">
-                <div
-                    class="container position-relative d-flex align-items-center justify-content-between"
-                >
-                    <a href="/" class="logo d-flex align-items-center">
-                        <!-- Uncomment the line below if you also wish to use an image logo -->
-                        <img
-                            src="{{ asset('assets/images/logo polresta.png') }}"
-                            alt=""
-                        />
-                        <img
-                            src="{{
-                                asset('assets/images/logo bebrayan3.png')
-                            }}"
-                            alt=""
-                        />
-                        <h4 class="sitename">
-                            Informasi Mudik Polresta Banyumas
-                        </h4>
-                        <span>.</span>
-                    </a>
-
-                    <nav id="navmenu" class="navmenu">
-                        <ul>
-                            <li>
-                                <a href="/" class="active">Home<br /></a>
-                            </li>
-                        </ul>
-                        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
-                    </nav>
+        <div id="carouselExample" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
+            <div class="carousel-inner">
+                <div class="carousel-item">
+                    <img src="{{ asset('assets/images/pospam/pospam ajibarang.jpg') }}"
+                        class="d-block w-100" alt="..." />
+                </div>
+                <div class="carousel-item active">
+                    <img src="{{ asset('assets/images/pospam/pospam kemranjen.jpg') }}"
+                        class="d-block w-100" alt="..." />
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ asset('assets/images/pospam/pospam wangon.jpg') }}"
+                        class="d-block w-100" alt="..." />
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ asset('assets/images/pospam/posyan alun-alun purwokerto.jpg') }}"
+                        class="d-block w-100" alt="..." />
                 </div>
             </div>
-        </header>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
 
-        <main class="main">
-            <div class="hero">
-                <div class="container section-title" data-aos="fade-up">
-                    <h2>Pos Pengamanan Mudik<br /></h2>
-                </div>
-
-                <div
-                    id="carouselExample"
-                    class="carousel slide"
-                    data-bs-ride="carousel"
-                    data-bs-interval="5000"
-                >
-                    <div class="carousel-inner">
-                        <div class="carousel-item">
-                            <img
-                                src="{{
-                                    asset(
-                                        'assets/images/pospam/pospam ajibarang.jpg'
-                                    )
-                                }}"
-                                class="d-block w-100"
-                                alt="..."
-                            />
-                        </div>
-                        <div class="carousel-item active">
-                            <img
-                                src="{{
-                                    asset(
-                                        'assets/images/pospam/pospam kemranjen.jpg'
-                                    )
-                                }}"
-                                class="d-block w-100"
-                                alt="..."
-                            />
-                        </div>
-                        <div class="carousel-item">
-                            <img
-                                src="{{
-                                    asset(
-                                        'assets/images/pospam/pospam wangon.jpg'
-                                    )
-                                }}"
-                                class="d-block w-100"
-                                alt="..."
-                            />
-                        </div>
-                        <div class="carousel-item">
-                            <img
-                                src="{{
-                                    asset(
-                                        'assets/images/pospam/posyan alun-alun purwokerto.jpg'
-                                    )
-                                }}"
-                                class="d-block w-100"
-                                alt="..."
-                            />
-                        </div>
-                    </div>
-                    <button
-                        class="carousel-control-prev"
-                        type="button"
-                        data-bs-target="#carouselExample"
-                        data-bs-slide="prev"
-                    >
-                        <span
-                            class="carousel-control-prev-icon"
-                            aria-hidden="true"
-                        ></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button
-                        class="carousel-control-next"
-                        type="button"
-                        data-bs-target="#carouselExample"
-                        data-bs-slide="next"
-                    >
-                        <span
-                            class="carousel-control-next-icon"
-                            aria-hidden="true"
-                        ></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
-            </div>
-
-            <div class="hero"></div>
-        </main>
-        
-
-        <!-- Scroll Top -->
-        <a
-            href="#"
-            id="scroll-top"
-            class="scroll-top d-flex align-items-center justify-content-center"
-            ><i class="bi bi-arrow-up-short"></i
-        ></a>
-
-        <!-- Preloader -->
-        <div id="preloader"></div>
-
-        <!-- Vendor JS Files -->
-        <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <script src="assets/vendor/php-email-form/validate.js"></script>
-        <script src="assets/vendor/aos/aos.js"></script>
-        <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-        <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-        <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-        <script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
-        <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-
-        <!-- Main JS File -->
-        <script src="assets/js/main.js"></script>
-    </body>
-</html>
+@endsection
